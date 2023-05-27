69b443a795559443963a20b4e4c65b7c618befef
Mauricio Portilla <portillarmz@gmail.com>
2023-05-27 13:55:04 -0600

    [Add] tests for communication with API

---
+++ tests/communication.py
@@ -0,0 +1,25 @@
+import requests
+from cleantext import clean
+
+url = "https://us-central1-srbanorte23.cloudfunctions.net/dialogflowGateway"
+msg = {"sessionId": "srbanorte23",
+        "queryInput": {
+            "text": {
+                "text": "Hola",
+                "languageCode": "es"
+            }
+        }}
+response = requests.post(url, json=msg)
+response = response.json()
+response_text = response["fulfillmentText"]
+print(response_text)
+
+print("-----------------------------")
+response_text_clean = clean(response_text, no_emoji=True).replace("\n", " ")
+print(response_text_clean)
+
+#print("-----------------------------")
+
+#print(repr(clean(response_text, no_emoji=True).strip()))
+
+
